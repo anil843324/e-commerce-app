aaa7d8942789c3daa34e1d23365ca142b4eb2d88
anil843324 <anilkrsingh14200@gmail.com>
2022-08-21 09:02:36 +0530

    some bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { HiOutlineSearch } from "react-icons/hi"
- import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 
- import {addToCart} from "../Redux/action/action"
- 
+import { addToCart } from "../Redux/action/action"
+
 const Home = () => {
 
-   const dispatch=useDispatch()
+  const dispatch = useDispatch()
   const [data, setData] = useState({})
-  const [input,setInput]=useState("")
-   const [filterKey,setFilterKey]=useState({
-      name:"",
-      flag:false
-   })
+  const [input, setInput] = useState("")
+  const [filterKey, setFilterKey] = useState({
+    name: "",
+    flag: false
+  })
+  const [priceFilter, setPriceFilter] = useState({
+    maxP: 0,
+    minP: 0
+  })
 
   useEffect(() => {
 
@@ -25,27 +29,42 @@ const Home = () => {
 
 
 
-//  handle serach data
+  //  handle serach data
 
- const handleSearchData=()=>{
+  const handleSearchData = () => {
 
 
- }
+  }
 
- // handle  checkbox 
- const handleChange = (e) => {
-	// Destructuring
-	const { value, checked } = e.target;
+  // handle  checkbox 
+  const handleChange = (e) => {
+    // Destructuring
+    const { value, checked } = e.target;
 
-	setFilterKey({
-    name:value,
-    flag:checked
+    setFilterKey({
+      name: value,
+      flag: checked
 
-  })
+    })
+    const [min, max] = value.split("-")
 
-};
+    if (max === undefined) {
+      setPriceFilter({
+        maxP: Number(min),
+        minP: 0
+      })
+    } else {
+      setPriceFilter({
+        maxP: Number(max),
+        minP: Number(min)
+      })
 
- console.log(filterKey);
+    }
+
+
+  };
+
+  console.log(filterKey);
 
 
 
@@ -61,116 +80,116 @@ const Home = () => {
           <div className='filterDiv bg-gray-300 w-[20%] h-[70vh] mt-20  drop-shadow-md rounded-md overflow-auto '>
 
             <div className=' flex flex-col ml-7  mt-5 text-xl gap-1 '>
-               <form >
-              <div >
-                <span>Colour</span>
-                <div className='flex  flex-col gap-1 ml-2'>
-                  <div className='flex gap-2'>
-                    <input 
-                     
-                    type="checkbox" value="Red" 
-                     className=' w-4 cursor-pointer '
-                     onChange={handleChange}
-                      />
-                    <label
-                  
-                     >Red</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input type="checkbox" 
-                    value="Blue" 
-                    onChange={handleChange}
-                    className=' w-4  cursor-pointer ' />
-                    <label
-                     
-                     > Blue</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input 
-                     value="Green" 
-                    onChange={handleChange}
-                    type="checkbox" className=' w-4  cursor-pointer ' />
-                    <label
-                   
-                     > Green</label>
-                  </div>
-                </div>
-              </div>
-              <div >
-                <span>Gender</span>
-                <div className='flex  flex-col gap-1 ml-2'>
-                  <div className='flex gap-2'>
-                    <input type="checkbox"
-                     value="Men" 
-                    onChange={handleChange}
-                     className=' w-4  cursor-pointer ' />
-                    <label 
-                    
-                    >Men</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input type="checkbox"
-                     value="Women" 
-                    onChange={handleChange}
-                     className=' w-4 cursor-pointer ' />
-                    <label > Women</label>
-                  </div>
+              <form >
+                <div >
+                  <span>Colour</span>
+                  <div className='flex  flex-col gap-1 ml-2'>
+                    <div className='flex gap-2'>
+                      <input
 
-                </div>
-              </div>
-              <div >
-                <span>Price</span>
-                <div className='flex  flex-col gap-1 ml-2'>
-                  <div className='flex gap-2'>
-                    <input type="checkbox"
-                     value="0-250" 
-                    onChange={handleChange}
-                     className=' w-4  cursor-pointer' />
-                    <label >0-Rs250</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input 
-                     value="251-450" 
-                    onChange={handleChange}
-                     type="checkbox" className=' w-4 cursor-pointer ' />
-                    <label > Rs251-450</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input
-                    value="450" 
-                    onChange={handleChange}
-                     type="checkbox" className=' w-4  cursor-pointer ' />
-                    <label > Rs 450</label>
-                  </div>
-                </div>
-              </div>
-              <div >
-                <span>Type</span>
-                <div className='flex  flex-col gap-1 ml-2'>
-                  <div className='flex gap-2'>
-                    <input 
-                    
-                    value="Polo" 
-                    onChange={handleChange}
-                     type="checkbox" className=' w-4 cursor-pointer ' />
-                    <label >Polo</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input type="checkbox" 
-                     value="Hoodie" 
-                    onChange={handleChange}
-                     className=' w-4 cursor-pointer ' />
-                    <label > Hoodie</label>
-                  </div>
-                  <div className='flex gap-2'>
-                    <input type="checkbox" 
-                     value="Basic" 
-                    onChange={handleChange}
-                    className=' w-4  cursor-pointer ' />
-                    <label > Basic</label>
+                        type="checkbox" value="Red"
+                        className=' w-4 cursor-pointer '
+                        onChange={handleChange}
+                      />
+                      <label
+
+                      >Red</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="Blue"
+                        onChange={handleChange}
+                        className=' w-4  cursor-pointer ' />
+                      <label
+
+                      > Blue</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input
+                        value="Green"
+                        onChange={handleChange}
+                        type="checkbox" className=' w-4  cursor-pointer ' />
+                      <label
+
+                      > Green</label>
+                    </div>
                   </div>
                 </div>
-              </div>
+                <div >
+                  <span>Gender</span>
+                  <div className='flex  flex-col gap-1 ml-2'>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="Men"
+                        onChange={handleChange}
+                        className=' w-4  cursor-pointer ' />
+                      <label
+
+                      >Men</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="Women"
+                        onChange={handleChange}
+                        className=' w-4 cursor-pointer ' />
+                      <label > Women</label>
+                    </div>
+
+                  </div>
+                </div>
+                <div >
+                  <span>Price</span>
+                  <div className='flex  flex-col gap-1 ml-2'>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="0-250"
+                        onChange={handleChange}
+                        className=' w-4  cursor-pointer' />
+                      <label >0-Rs250</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input
+                        value="251-450"
+                        onChange={handleChange}
+                        type="checkbox" className=' w-4 cursor-pointer ' />
+                      <label > Rs251-450</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input
+                        value="450"
+                        onChange={handleChange}
+                        type="checkbox" className=' w-4  cursor-pointer ' />
+                      <label > Rs 450</label>
+                    </div>
+                  </div>
+                </div>
+                <div >
+                  <span>Type</span>
+                  <div className='flex  flex-col gap-1 ml-2'>
+                    <div className='flex gap-2'>
+                      <input
+
+                        value="Polo"
+                        onChange={handleChange}
+                        type="checkbox" className=' w-4 cursor-pointer ' />
+                      <label >Polo</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="Hoodie"
+                        onChange={handleChange}
+                        className=' w-4 cursor-pointer ' />
+                      <label > Hoodie</label>
+                    </div>
+                    <div className='flex gap-2'>
+                      <input type="checkbox"
+                        value="Basic"
+                        onChange={handleChange}
+                        className=' w-4  cursor-pointer ' />
+                      <label > Basic</label>
+                    </div>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
@@ -186,11 +205,11 @@ const Home = () => {
 
 
                 <input type="text" name="" placeholder='Search for Products'
-                 value={input}
-                onChange={ (e)=> {setInput(e.target.value)} }
-                 className='outline-none p-2 w-80 border-b-2 border-gray-400 ' />
-                <span className=' bg-slate-400 text-white px-4 py-3  rounded-md cursor-pointer ' 
-                  onClick={()=> handleSearchData()}
+                  value={input}
+                  onChange={(e) => { setInput(e.target.value) }}
+                  className='outline-none p-2 w-80 border-b-2 border-gray-400 ' />
+                <span className=' bg-slate-400 text-white px-4 py-3  rounded-md cursor-pointer '
+                  onClick={() => handleSearchData()}
                 >
                   <HiOutlineSearch size={20} />
                 </span>
@@ -204,30 +223,32 @@ const Home = () => {
 
               {
 
-                data.length>0 && data.filter((obj) => {
-                  if(filterKey.flag===false){
+                data.length > 0 && data.filter((obj) => {
+                  if (filterKey.flag === false) {
                     return obj;
-                  }else if(obj.gender.includes(filterKey.name)){
+                  } else if (obj.gender.includes(filterKey.name)) {
                     return obj;
-                  }else if(obj.color.includes(filterKey.name)){
+                  } else if (obj.color.includes(filterKey.name)) {
                     return obj;
-                  }else if(obj.name.includes(filterKey.name)){
+                  } else if (obj.name.includes(filterKey.name)) {
                     return obj;
-                  }
-                }).map((ele)=>(
-                  <div className='  pt-3  px-4 h-[245px] w-[220px]   border border-indigo-600 ' key={ele.id} >
-                <div >
-                  <img src={ele.imageURL} alt="product img" className=' w-[180px] h-[180px]' />
-                </div>
+                  } else if (  (obj.price >= priceFilter.minP && obj.price <= priceFilter.maxP)   ){
+                    return obj;
+                  } 
+                }).map((ele) => (
+                    <div className='  pt-3  px-4 h-[245px] w-[220px]   border border-indigo-600 ' key={ele.id} >
+                      <div >
+                        <img src={ele.imageURL} alt="product img" className=' w-[180px] h-[180px]' />
+                      </div>
 
-                <div className=' flex  justify-between items-center mt-2 mb-2 '>
-                  <span className='ml-5'>Rs {ele.price} </span>
-                  <span className='bg-slate-600 text-white p-1  rounded-md  cursor-pointer '  onClick={ ()=> {dispatch(addToCart(ele))} }     > Add to cart </span>
-                </div>
-              </div>
-                ))
+                      <div className=' flex  justify-between items-center mt-2 mb-2 '>
+                        <span className='ml-5'>Rs {ele.price} </span>
+                        <span className='bg-slate-600 text-white p-1  rounded-md  cursor-pointer ' onClick={() => { dispatch(addToCart(ele)) }}     > Add to cart </span>
+                      </div>
+                    </div>
+                  ))
               }
-            
+
 
             </div>
 
